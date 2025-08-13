6febf753f5b06dcaa0eb612af25fd189b192ba6a
Erick <erickcamposdev@gmail.com>
2025-08-13 20:37:23 -0300

    feat: create project

---
+++ HostApp/rspack.config.mjs
@@ -0,0 +1,67 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import * as Repack from '@callstack/repack';
+import {withZephyr} from 'zephyr-repack-plugin';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Rspack configuration enhanced with Re.Pack defaults for React Native.
+ *
+ * Learn about Rspack configuration: https://rspack.dev/config/
+ * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
+ */
+
+export default withZephyr()(env => {
+  const {platform, mode} = env;
+  return {
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      // 1. Understand the file path of ios and android file extensions
+      // 2. Configure the output to be as close to Metro as possible
+      ...Repack.getResolveOptions(),
+    },
+    output: {
+      // Unsure - for module federation HMR and runtime?
+      uniqueName: 'react-native-host-app',
+    },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules(),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin({
+        platform,
+      }),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'HostApp',
+        filename: 'HostApp.container.js.bundle',
+        dts: false,
+        remotes: {
+          MiniApp: `MiniApp@http://localhost:9001/${platform}/MiniApp.container.js.bundle`,
+        },
+        shared: {
+          react: {
+            singleton: true,
+            version: '19.0.0',
+            eager: true,
+          },
+          'react-native': {
+            singleton: true,
+            version: '0.78.0',
+            eager: true,
+          },
+        },
+      }),
+      // Supports for new architecture - Hermes can also use JS, it's not a requirement, it will still work the same but it's for performance optimization
+      new Repack.plugins.HermesBytecodePlugin({
+        enabled: mode === 'production',
+        test: /\.(js)?bundle$/,
+        exclude: /index.bundle$/,
+      }),
+    ],
+  };
+});
---
+++ MiniApp/rspack.config.mjs
@@ -0,0 +1,65 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import * as Repack from '@callstack/repack';
+import {withZephyr} from 'zephyr-repack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const STANDALONE = Boolean(process.env.STANDALONE);
+
+/**
+ * Rspack configuration enhanced with Re.Pack defaults for React Native.
+ *
+ * Learn about Rspack configuration: https://rspack.dev/config/
+ * Learn about Re.Pack configuration: https://re-pack.dev/docs/guides/configuration
+ */
+
+export default withZephyr()(env => {
+  const {platform, mode} = env;
+  return {
+    mode,
+    context: __dirname,
+    entry: './index.js',
+    resolve: {
+      ...Repack.getResolveOptions(),
+    },
+    output: {
+      uniqueName: 'react-native-mini-app',
+    },
+    module: {
+      rules: [
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules({inline: true}),
+      ],
+    },
+    plugins: [
+      new Repack.RepackPlugin(),
+      new Repack.plugins.ModuleFederationPluginV2({
+        name: 'MiniApp',
+        filename: 'MiniApp.container.js.bundle',
+        dts: false,
+        exposes: {
+          './App': './App.tsx',
+        },
+        shared: {
+          react: {
+            singleton: true,
+            version: '19.0.0',
+            eager: STANDALONE,
+          },
+          'react-native': {
+            singleton: true,
+            version: '0.78.0',
+            eager: STANDALONE,
+          },
+        },
+      }),
+      new Repack.plugins.HermesBytecodePlugin({
+        enabled: mode === 'production',
+        test: /\.(js)?bundle$/,
+        exclude: /index.bundle$/,
+      }),
+    ],
+  };
+});
